04b64bc00894487d5a9af09b135688096873467a
LymarN <63715673+LymarN@users.noreply.github.com>
2020-04-27 12:54:49 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package domain;
 
+/**
+ *
+ * @author Надюха
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+   public static int Calculate() {
+    
+       /**
+        * formula for calculating the Armstrong number
+        */
+    int num = 548;
+    int temp = num;
+    int res, summa = 0;
+    while(num > 0){
+        res = num % 10;
+       num = num / 10;
+        summa = summa +  res*res*res;   
     }
+    /**
+     * checking numbers as armstrong numbers
+     * if the number meets the condition, we get a confirmation
+     */
+    if(temp != summa)
+        System.out.println("This number is not Armstrong number");
+    else 
+        System.out.println("Its an Armstrong number");
+    return temp ;
+}
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,16 @@
+
 package test;
 
 import domain.Exercise;
-
+/**
+ *
+ * @author Надюха
+ */
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+/**
+ * print the result to the screen
+ */
+        System.out.println("Your number: " + Exercise.Calculate()); 
     }
 }
